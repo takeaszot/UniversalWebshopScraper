caf77cffff3e46549f02a5bf8ea9e119669fcf27
JMaskiewicz <j.maskiewicz@student.uw.edu.pl>
2025-01-13 20:09:53 +0100

    lawnmower added

--- UniversalWebshopScraper/generalized_scrapper/core/product_categories.py
+++ UniversalWebshopScraper/generalized_scrapper/core/product_categories.py
@@ -1,6 +1,15 @@
 # product_categories.py
 
 categories_products = {
+    "Lawnmower": [
+        "lawnmower", "Autonomous lawnmower", "Robotic lawnmower", "Self-driving lawnmower", "Automatic lawnmower", "Smart lawnmower",
+        "AI lawnmower", "GPS lawnmower", "Electric lawnmower", "Battery-powered lawnmower", "Cordless lawnmower",
+        "Remote control lawnmower", "Solar-powered lawnmower", "Husqvarna Automower", "Bosch robotic lawnmower",
+        "Worx Landroid", "Lawn mowing robot", "Smart yard robot", "Zero-turn lawnmower", "Push lawnmower",
+        "Ride-on lawnmower", "Gas-powered lawnmower", "Mulching lawnmower", "Lawn tractor", "Grass cutting robot",
+        "Quiet lawnmower"
+    ],
+
     "Electronics": [
         "Wireless Bluetooth Earbuds", "4K UHD Smart TV", "Noise-Cancelling Headphones",
         "Portable Power Bank", "Smartphone with Triple Camera", "Smartwatch with Fitness Tracker",
