db5ff1912eb4947c9e80e2ef274baad6046a23a8
JMaskiewicz <j.maskiewicz@student.uw.edu.pl>
2025-01-24 21:22:33 +0100

    model v1.3

--- UniversalWebshopScraper/single_product_scrapper/core/single_generalized_scrapper.py
+++ UniversalWebshopScraper/single_product_scrapper/core/single_generalized_scrapper.py
@@ -5,6 +5,8 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 
+from vectorproduct.utils.time_decorators import time_it
+
 
 class SingleProductGeneralizedScraper:
     def __init__(self, shopping_website, home_page):
@@ -166,9 +168,6 @@ class SingleProductGeneralizedScraper:
 # EXAMPLE USAGE
 # ================================================================
 if __name__ == "__main__":
-    # -------------------------
-    # eBay example
-    # -------------------------
     print("eBay example:")
     scraper = SingleProductGeneralizedScraper(
         shopping_website="https://www.ebay.com",
@@ -178,15 +177,16 @@ if __name__ == "__main__":
     home_url = "https://www.ebay.com"
     product_url = ("https://www.ebay.com/itm/387423894005?_trkparms=amclksrc%3DITM%26aid%3D1110013%26algo%3DHOMESPLICE.SIMRXI%26ao%3D1%26asc%3D20230918095141%26meid%3Da1a8c4dc4fda48ffb8f1a3dfc529fb65%26pid%3D101843%26rk%3D2%26rkt%3D23%26itm%3D387423894005%26pmt%3D1%26noa%3D0%26pg%3D4375194%26algv%3DSimRXIV2BaseWithHPPrerankerOptimizedFilteringRVICIIEmbeddingRecall%26brand%3DUnbranded&_trksid=p4375194.c101843.m3021&itmprp=cksum%3A387423894005a1a8c4dc4fda48ffb8f1a3dfc529fb65%7Cenc%3AAQAJAAABMPIUntTq2Gv6ns2UEoKw%252B6dEe2ar%252F1a0hFj0eXUL%252FQmvyFLcIe7Isl%252BBv6BPSbBg8bufZGKmIQY7seLe03p9IC4wJJRkmxMJ695EaIrGN%252FtVWXWnkurL0JSc41%252Br4oqA0%252FOaFA1yc8hdOUbZ6Bt4T04%252BjBVySvxf8jbLaU%252BQuTHgtJZQI7S4QwvHptZ5Z6EXxttWppFvCETEstY9736BRCOgfEgFVEucrZWMCiUCSt7PI7ms8iVp7mGsU4Dwlr4rxZxhFGnn5GRveqc8aDIjPtCwoxTnYoBtPnbpL%252F%252Bic70COgPHdqVNsc0phwRdngdmE79JyG5Tm3qgRiUexp1gsUloCa2yrBItKgIOFYVOC9cQri4M3Ei%252BLzB%252FWX8NzK18Eetv2pkvBbd%252BDstRayVPaRg%253D%7Campid%3APL_CLK%7Cclp%3A4375194&itmmeta=01JGKG8TQ1992WA3K6XRRD61P4&_trkparms=parentrq%3A2704695c1940a626cd9fe5d3ffffd74a%7Cpageci%3A2fe2e456-c906-11ef-9a78-2e2a4490a4fb%7Ciid%3A1%7Cvlpname%3Avlp_homepage")  # example product
 
-    # Detect number of smallest HTML blocks (leaf nodes)
-    scraper.detect_number_of_HTML_blocks(home_url, product_url)
+    # Create a DataFrame of leaf nodes with classification
+    df_ebay = scraper.make_leaf_nodes_df(home_url, product_url)
+    print(df_ebay.head(20))  # show first 20 rows
+    print("\nNumber of rows in df:", len(df_ebay))
+
+    # Optionally scrape comments
     scraper.scrape_comments_for_product(home_url, product_url)
     scraper.close_driver()
-    print("\namazon example:")
 
-    # -------------------------
-    # Amazon example
-    # -------------------------
+    print("\nAmazon example:")
     scraper = SingleProductGeneralizedScraper(
         shopping_website="https://www.amazon.com",
         home_page="https://www.amazon.com"
@@ -195,8 +195,11 @@ if __name__ == "__main__":
     home_url = "https://www.amazon.com"
     product_url = ("https://www.amazon.com/AmazonBasics-Extra-Thick-Exercise-Carrying/dp/B01LP0U5X0/?_encoding=UTF8&pd_rd_w=kEPhA&content-id=amzn1.sym.9929d3ab-edb7-4ef5-a232-26d90f828fa5&pf_rd_p=9929d3ab-edb7-4ef5-a232-26d90f828fa5&pf_rd_r=B4BFFWFE0Z18GNTAJB5G&pd_rd_wg=UPiln&pd_rd_r=e771d60f-1763-4ae6-adea-227cce6d5f98&ref_=pd_hp_d_btf_crs_zg_bs_3375251&th=1")
 
+    # Create a DataFrame of leaf nodes with classification
+    df_amazon = scraper.make_leaf_nodes_df(home_url, product_url)
+    print(df_amazon.head(20))  # show first 20 rows
+    print("\nNumber of rows in df:", len(df_amazon))
 
-    # Scrape comments for a product
-    scraper.detect_number_of_HTML_blocks(home_url, product_url)
+    # Optionally scrape comments
     scraper.scrape_comments_for_product(home_url, product_url)
     scraper.close_driver()
